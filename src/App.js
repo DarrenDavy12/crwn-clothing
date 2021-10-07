9716a27cd7b423cafbc4381b63e85311f3ad5a4f
DarrenDavy12 <darrendavy123@gmail.com>
2021-10-07 12:41:06 +0100

    create react app template with added files for homepage

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Homepage from './components/homepage.component';
+import './App.css';
+
+
+function App() {
+  return <div>
+    <Homepage />
+  </div>
+};
+
+export default App;
